939516aefd073fb1c75b01985b7f37ae0c629bd6
jasmin1002 <jasmineolofin@gmail.com>
2022-03-10 20:43:03 +0000

    C program that uses puts() to print to stdout

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * puts usage
+ *
+ * return 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
