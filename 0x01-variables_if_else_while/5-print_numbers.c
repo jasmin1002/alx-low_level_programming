0516c800ffc9edae1c8fc78b7cde04351aba7c2e
jasmin1002 <jasmineolofin@gmail.com>
2022-03-11 18:26:36 +0000

    Program prints all single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Author: Olalekan js
+ * Nick	 : .Files
+ * Date	 : 11-03-2022
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+
+	printf("\n");
+
+
+	return (0);
+}
