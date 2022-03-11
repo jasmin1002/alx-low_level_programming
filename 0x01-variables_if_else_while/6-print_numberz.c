130134a8d2e034b3ff55a55bf184676f55e0f1b9
jasmin1002 <jasmineolofin@gmail.com>
2022-03-11 21:20:35 +0000

    C program prints each single digit of denary numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ *
+ * Author: Olalekan js
+ * Nick  : .Files
+ * Date  : 11-03-2022
+ *
+ */
+int main(void)
+{
+	int gt;
+
+	gt = 48;
+
+	while (gt <= 57)
+	{
+		putchar(gt);
+		gt++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
