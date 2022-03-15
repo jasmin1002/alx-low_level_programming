4ea3b0973d4813a5d0e1933e9b09af454974d3e8
jasmin1002 <jasmineolofin@gmail.com>
2022-03-15 15:53:33 +0000

    Function checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+/**
+ * _isalpha - checks for alphabetic character.
+ *
+ * @c: The input alphabetic character to check
+ *
+ * Author: Olalekan js
+ * Nick  : .Files
+ * Date  : 15-03-2022
+ *
+ * Return: 1 for alphabetic character, 0 for non-alphabetic character
+ *
+ */
+int _isalpha(int c)
+{
+	/** ret - for result, stores the value of validation **/
+	int ret;
+
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		ret = 1;
+
+	else
+		ret = 0;
+
+	return (ret);
+}
