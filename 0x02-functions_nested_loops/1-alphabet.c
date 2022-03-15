5c73e0eff16cab2b680b415b8dfecf2d1c6523a7
jasmin1002 <jasmineolofin@gmail.com>
2022-03-15 13:42:27 +0000

    This function prints the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+/** Referenced _putchar function */
+int _putchar(char);
+/**
+ * print_alphabet - print alphabet stdout
+ *
+ * Author: Olalekan js
+ * Nick  : .Files
+ * Date  : 15-03-2022
+ *
+ * Return: void
+ *
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	/** Assign 'a' 97 (ASCII) */
+	c = 97;
+
+	while (c <= 122)
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+}
+
