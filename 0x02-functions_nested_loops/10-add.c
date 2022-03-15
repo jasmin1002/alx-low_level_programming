9e58cd37498f0e222d3fd48c1e3bb2b39b2a4f60
jasmin1002 <jasmineolofin@gmail.com>
2022-03-15 21:48:09 +0000

    Function adds two integers and results the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - performs addition on two given arguments
+ *
+ * @n: first operand
+ * @m: second operand
+ *
+ * Author: Olalekan js
+ * Nick  : .Files
+ * Date  : 15-02-2022
+ *
+ * Return: Always return sum of n + m
+ *
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
