eb7fcbef263ab8633636e6c6df536dfe9d26182d
jasmin1002 <jasmineolofin@gmail.com>
2022-03-16 04:34:24 +0000

    jack bauer added

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - prints mins of the day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int m;
+	int n;
+
+	while (m < 24)
+	{
+		
+		n = 0;
+		while (n < 60)
+		{
+			_putchar(m/10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar(':');
+			_putchar(n/10 + '0');
+			_putchar(n%10 + '0');
+			_putchar('\n');
+
+			n++;
+		}
+		
+		m++;
+}
