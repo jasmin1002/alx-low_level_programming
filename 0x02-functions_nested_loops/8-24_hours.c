a43aa9b8a840314ddec3c8e2ac5c056f2e4509f9
jasmin1002 <jasmineolofin@gmail.com>
2022-03-16 04:45:14 +0000

    Jack bauer's file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,16 +9,18 @@ void jack_bauer(void)
 	int m;
 	int n;
 
+	m = 0;
+
 	while (m < 24)
 	{
 		n = 0;
 		while (n < 60)
 		{
-			_putchar(m / 10 + '0');
-			_putchar(m % 10 + '0');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar(':');
-			_putchar(n / 10 + '0');
-			_putchar(n % 10 + '0');
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
 			_putchar('\n');
 
 			n++;
