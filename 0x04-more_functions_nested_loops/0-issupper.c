61a8ddb971ab4849001af788db0ef321c3b63c10
jasmin1002 <jasmineolofin@gmail.com>
2022-03-17 12:40:52 +0000

    function decides if an input is a uppercase or not

---
+++ 0x04-more_functions_nested_loops/0-issupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isupper - checks if an input character is a UPPERCASE letter.
+ * @c: placeholder for input character.
+ *
+ * Return: 1 for UPPERCASE letter, 0 otherwise
+ *
+ */
+int _isupper(int c)
+{
+	int ret;
+
+	/* if an input is uppercase letter, return 0 */
+	if (!(c >= 65 && c <= 90))
+		ret = 0;
+
+	/* if an input is other than uppercase letter, return 1 */
+	else
+		ret = 1;
+
+	/* return ret base on the assignment */
+	return (ret);
+}
