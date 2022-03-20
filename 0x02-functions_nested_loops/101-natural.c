1fc309255c57b6edb42599cb9d35484f8bab1889
jasmin1002 <jasmineolofin@gmail.com>
2022-03-20 19:09:59 +0000

    101-natural.c modified

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,9 +5,35 @@
  *
  * Return: 0 (success)
  */
+
+void print_sum_of_3or5(int);
+
 int main(void)
 {
 	print_sum_of_3or5(1024);
 
 	return (0);
+}
+
+void print_sum_of_3or5(int n)
+{
+	if (n >= 3 || n >= 5)
+	{
+		int sum = 0;
+
+		int inc;
+
+		for (inc = 1; inc < n; inc++)
+		{
+			/**
+			 * check if n, at every iteration,
+			 * is divisible by 3 or 5
+			 */
+			if (inc % 3 == 0 || inc % 5 == 0)
+				sum = sum + inc;
+
+		}
+
+		printf("Sum equals %d\n", sum);
+	}
 }
