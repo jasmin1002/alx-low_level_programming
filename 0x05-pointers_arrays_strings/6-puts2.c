23f86e85a36acb4fc0143cbb631079c833fe1662
jasmin1002 <jasmineolofin@gmail.com>
2022-03-21 23:16:27 +0000

    comments are added to line of codes

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,15 +9,19 @@
  */
 void puts2(char *str)
 {
+	/* s: is a pointer of pointer */
 	char **s, *iterator;
 	int len, i;
 
+	/* s: holds reference to address of memory str */
 	s = &str;
 
+	/* Assign value of input string to iterator */
 	iterator = *s;
 
 	len = strlen(*s) - 1;
 
+	/* inc by step of 2 */
 	for (i = 0; i <= len; i = i + 2)
 	{
 		_putchar(iterator[i]);
