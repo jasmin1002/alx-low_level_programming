2d1b7fb27fa6f0ccb9b9a0068a17fd6ff31f9b32
jasmin1002 <jasmineolofin@gmail.com>
2022-03-23 12:14:06 +0000

    main.h file updated and new file 1-strncat.c added

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strncat - joins two strings together
+ * @dest: first formal string input
+ * @src:  second formal string input
+ * @n:    size or length of the src (formal) input
+ *
+ * Return: concatenated string stores in @dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	int len = strlen(dest);
+
+	while (i != n)
+	{
+		if (i < n)
+			dest[len + i] = *(src + i);
+
+		/*else
+			dest[len + i] = '\0';*/
+
+		i++;
+	}
+
+	return (dest);
+}
+	
