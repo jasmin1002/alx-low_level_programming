ea20bcb17d33654fa7c04f0cd5f1b418d2ea3fee
jasmin1002 <jasmineolofin@gmail.com>
2022-03-28 14:40:52 +0000

    2-strchr.c and main.h
    2-strchr.c file has C program and main.h file updated

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strchr - searches for a character in a string.
+ * @s: formal argument for input string.
+ * @c: character to look for in a string.
+ *
+ * Return: pointer to character or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	char *p = &c;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			p = s;
+
+			break;
+		}
+		
+		p = '\0';
+
+
+		s++;
+	}
+
+	return (p);
+}
