bd2344d854836c9e264abce7cbcfebca9f439bc5
jasmin1002 <jasmineolofin@gmail.com>
2022-03-28 16:55:28 +0000

    Lines of codes commented

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,10 +10,20 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i = 0;
 
+	/**
+	 * Iterate ONLY through the first part/chunk of a given string
+	 * SPACE serves as delimiter
+	 */
 	while (*s != '\0')
 	{
 		int j = 0;
 
+		/**
+		 * For each character of given string, check if it has
+		 * any of the character(s) in accept variable
+		 * If there is, increment i by 1 for every of occurrence
+		 * character in accept that matches character in s
+		 */
 		while (*(accept + j) != '\0')
 		{
 			if (*s == *(accept + j))
@@ -22,6 +32,7 @@ unsigned int _strspn(char *s, char *accept)
 			j++;
 		}
 
+		/* If there is SPACE delimiter, break out of the loop */
 		if (*s == ' ')
 			break;
 
