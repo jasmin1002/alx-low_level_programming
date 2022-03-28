eb3463e6dd7ce17e5ab8c337ed7c9fed15ae89dd
jasmin1002 <jasmineolofin@gmail.com>
2022-03-28 19:30:05 +0000

    7-print_chessboard.c file added and main.h file updated

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,3 +3,6 @@ char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
 unsigned int _strspn(char *, char *);
 char *_strpbrk(char *, char *);
+char *_strstr(char *, char *);
+void print_chessboard(char (*a)[8]);
+int _putchar(char);
