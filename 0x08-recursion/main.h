a84cc18895a0252e05b8821990e7c134a159c606
jasmin1002 <jasmineolofin@gmail.com>
2022-03-30 21:46:22 +0000

    main.h updated and string reverse by recursion added

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,5 +2,6 @@ void _puts_recursion(char *);
 int _putchar(char);
 void _print_rev_recursion(char *);
 int _strlen_recursion(char *);
-int factorial(int n);
-int _pow_recursion(int x, int y);
+int factorial(int);
+int _pow_recursion(int, int);
+int is_prime_number(int);
