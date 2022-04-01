9d2cd8fc85cba7406c4ca6fab38d6af3d45c54d9
jasmin1002 <jasmineolofin@gmail.com>
2022-04-01 12:30:25 +0000

    Empty lines removed in main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char);
 int _islower(int);
 int _isalpha(int);
@@ -21,5 +20,4 @@ char *_strchr(char *, char);
 unsigned int _strspn(char *, char *);
 char *_strpbrk(char *, char *);
 char *_strstr(char *, char *);
-
 #endif
