f06e37f3f3b158e268df9a703a208c627db38ce4
jasmin1002 <jasmineolofin@gmail.com>
2022-04-04 13:48:20 +0000

    Null corrected

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,37 +10,33 @@
  */
 char *_strdup(char *str)
 {
-	/**
-	 * i: incremental var
-	 * size: stores the length of the given string
-	 * str_arr: stores the array of the copy string
-	 */
-	unsigned int i = 0;
-	size_t size = strlen(str);
+	int i;
+	size_t size;
 	char *str_arr;
 
 	if (str == NULL)
 		return (NULL);
 
-	else
+	i = 0;
+	size = strlen(str);
+
+	/* size is used to know exactly the amount of memory to keep */
+	str_arr = (char *)malloc(size * sizeof(char) + 1);
+
+	if (str_arr == NULL)
+		return ('\0');
+
+	/* Initialize the memory space by copying the given string */
+	while (*str != '\0')
 	{
-		/* size is used to know exactly the amount of memory to keep */
-		str_arr = (char *)malloc(size * sizeof(char) + 1);
+		*(str_arr + i) = *(str++);
 
-		if (str_arr == NULL)
-			return ('\0');
-
-		/* Initialize the memory space by copying the given string */
-		while (*str != '\0')
-		{
-			*(str_arr + i) = *(str++);
-
-			i++;
-		}
-
-		/* End the copied string with NULL terminating character */
-		*(str_arr + i) = '\0';
+		i++;
 	}
+
+	/* End the copied string with NULL terminating character */
+	*(str_arr + i) = '\0';
+
 
 	return (str_arr);
 
