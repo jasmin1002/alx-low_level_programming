4166bd98956f742cdff816b22e8174691c0f5755
jasmin1002 <jasmineolofin@gmail.com>
2022-04-04 14:56:22 +0000

    concat string file has comments and allocated memory size adjusted

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,13 +16,19 @@ char *str_concat(char *s1, char *s2)
 	size_t size1, size2;
 	char *cat_str;
 
+	/* If passed value is NULL, treat it as string */
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
 
-	size1 = strlen(s1) + 1;
+	/**
+	 * Otherwise, find the size of the string so as to find
+	 * the size of allocated memory. Note +1 becos concat string needs
+	 * one terminating character in eventually
+	 */
+	size1 = strlen(s1);
 	size2 = strlen(s2) + 1;
 
 	cat_str = (char *)malloc(sizeof(char) * (size1 + size2));
@@ -30,10 +36,10 @@ char *str_concat(char *s1, char *s2)
 	if (cat_str == NULL)
 		return (NULL);
 
-	for (i = 0; *s1 != '\0'; i++)
+	for (i = 0; *s1 != '\0'; i++)	/* Copy the 1st input */
 		cat_str[i] = *(s1++);
 
-	for (; *s2 != '\0'; i++)
+	for (; *s2 != '\0'; i++)	/* Then, copy last the 2nd input */
 		cat_str[i] = *(s2++);
 
 	cat_str[i] = '\0';
