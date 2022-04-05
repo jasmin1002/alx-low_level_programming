1e35348666fca2dee95b58da0f8cec128810cb8e
jasmin1002 <jasmineolofin@gmail.com>
2022-04-05 15:18:11 +0000

    memory issue

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,14 +16,14 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = (int **)malloc(sizeof(int) * width);
+	grid = malloc(sizeof(int *) * width);
 
 	if (grid == NULL)
 		return (NULL);
 
 	for (i = 0; i < width; i++)
 	{
-		ptrArrInt = (int *)malloc(sizeof(int) * height);
+		ptrArrInt = malloc(sizeof(int) * height);
 
 		if (ptrArrInt == NULL)
 			return (NULL);
