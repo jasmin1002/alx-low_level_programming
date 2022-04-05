6717b5f054f51684c97c585b05a76ba210c3cf72
jasmin1002 <jasmineolofin@gmail.com>
2022-04-05 16:13:34 +0000

    mem-size tweaked

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,14 +16,14 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = malloc(sizeof(int *) * width);
+	grid = malloc(sizeof(int *) * height);
 
 	if (grid == NULL)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		ptrArrInt = malloc(sizeof(int) * height);
+		ptrArrInt = malloc(sizeof(int) * width);
 
 		if (ptrArrInt == NULL)
 			return (NULL);
