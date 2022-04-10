4c85ac4ada41a60ea2f91083ac85c8ff36ab1c24
jasmin1002 <jasmineolofin@gmail.com>
2022-04-10 22:49:33 +0000

    testing concatenate and stores to new variable args input with program name

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,17 +12,20 @@ char *argstostr(int ac, char **av)
 {
 	char *str;
 	int i, j, len = 0;
+	int indexLen = 0;
 
 	/* If inputs ac and av are 0 and NULL respectively, return NULL */
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	/* Get the total length of all the arguments altogether */
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 		len += strlen(av[i]);
-	
+
+	len = len + ac;
+
 	/* Use the total length of all arguments to make the memory size */
-	str = (char *)malloc((sizeof(char) * len) + ac);
+	str = (char *)malloc((sizeof(char) * len) + sizeof(char));
 	if (str == NULL)	/* If no space available, return NULL */
 	{
 		free(str);
@@ -30,19 +33,20 @@ char *argstostr(int ac, char **av)
 	}
 
 	/* Iterate to initialize and copy to the content of str */
-	for (j = 0, i = 1; j < len + ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		char *s = av[i];
 
-		for (; *(s + j) != '\0';)
-		{
-			str[j] = *(s + j);
-			j++;
-		}
-		
-		/*if (*s == '\0')*/
-		str[j] = '\n';
+		for (j = 0; (str[indexLen + j] = *(s++)); j++)
+			;
+
+		str[indexLen + j] = '\n';
+
+		indexLen += j + 1;
+
 	}
+
+	str[indexLen + 1] = '\0';
 
 
 	return (str);
