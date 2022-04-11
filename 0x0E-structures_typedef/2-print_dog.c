ea7153e203adb117f37b016f9bd090dc23274f98
jasmin1002 <jasmineolofin@gmail.com>
2022-04-11 17:08:08 +0000

    print formats to (nil)

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,8 +13,8 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	printf("Name: %s\n", d->name ? d->name : "nil");
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %.6f\n", d->age ? d->age : 0);
-	printf("Owner: %s\n", d->owner ? d->owner : "nil");
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 
 }
