4ffd93232b675940ed11cbcc98101454fa30ac17
jasmin1002 <jasmineolofin@gmail.com>
2022-04-11 16:27:24 +0000

    header file updated with print_dog proto and 2-print_dog displays dog struct contents

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - displays the elements in struct dog.
+ * @d: pointer to dog structure.
+ *
+ * Return: void (nothing).
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("nil\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	if (d->age == 0)
+		printf("nil\n");
+	else
+		printf("Age: %.6f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("nil\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
