1a084bc906d9488673910560a0606ab317139751
jasmin1002 <jasmineolofin@gmail.com>
2022-04-11 14:32:00 +0000

    dog structure header file added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - short description
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: dog's owner.
+ *
+ * Description: i declared a structure with mixed data types of char pointer and float
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
