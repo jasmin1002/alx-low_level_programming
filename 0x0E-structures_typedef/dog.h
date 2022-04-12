3057db236519fcf95aaf4104df5a7ab47f10229d
jasmin1002 <jasmineolofin@gmail.com>
2022-04-12 04:59:26 +0000

    files added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -25,5 +25,6 @@ typedef struct dog dog_t;
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
 dog_t *new_dog(char *, float, char *);
+void free_dog(dog_t *);
 
 #endif
