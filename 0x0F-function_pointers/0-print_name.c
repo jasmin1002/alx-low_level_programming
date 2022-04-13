690e87b4c5fd6bb9bc0f341ca5ab875113e9d450
jasmin1002 <jasmineolofin@gmail.com>
2022-04-13 10:34:07 +0000

    Header files defines the func proto and print_name displays by callback

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function pointer
+ *
+ * Return: Nothing (void)
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
