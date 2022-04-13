b54efc8245061eab25c56f27dc8527fc915749b9
jasmin1002 <jasmineolofin@gmail.com>
2022-04-13 11:58:03 +0000

    Null checking compact

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,12 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (action != NULL)
+	if (action != NULL && array != NULL)
 	{
 		for (i = 0; i < size; i++)
-		{
-			if (array != NULL)
-				action(*(array++));
-		}
+			action(*(array++));
 	}
 }
