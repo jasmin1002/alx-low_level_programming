ba16b4688cbd62279847cbd86dd1b96e5fd7a2df
jasmin1002 <jasmineolofin@gmail.com>
2022-04-13 10:59:22 +0000

    header file updated with array_iterator proto and array_iterator loops the given array

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
+#include <stdlib.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
 void print_name(char *, void (*)(char *));
+void array_iterator(int *, size_t, void (*)(int));
 
 #endif
