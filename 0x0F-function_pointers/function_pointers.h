67c64f1086b97f8b7f67f24137fa85ff797c33c8
jasmin1002 <jasmineolofin@gmail.com>
2022-04-13 11:08:48 +0000

    header file restructured and print_name adjusted with parameter checking

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
-#include <stdlib.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stdlib.h>
 void print_name(char *, void (*)(char *));
 void array_iterator(int *, size_t, void (*)(int));
 
