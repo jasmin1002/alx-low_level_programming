8b1e898a6fc33e9bf461c3000d46bb1cc638316f
jasmin1002 <jasmineolofin@gmail.com>
2022-04-14 04:46:55 +0000

    segfault fixed in 3-get_op_func

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,14 +20,14 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (*(ops + i)->op)
+	while (ops[i].op)
 	{
 		/**
 		 * if ((*s == *(ops + i)->op) == 0)
 		 *	return (*(ops + i)->f);
 		 */
 		if (strcmp(s, ops[i].op) == 0)
-			return (*(ops + i)->f);
+			return (ops[i].f);
 
 		i++;
 	}
