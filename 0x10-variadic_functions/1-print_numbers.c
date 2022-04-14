a3906c366c713fd4a7971de6e51a7b99f5ced7b2
jasmin1002 <jasmineolofin@gmail.com>
2022-04-14 23:19:13 +0000

    parameter check of number of variable input count

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,16 +15,19 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (separator != NULL)
 	{
-		va_start(ap, n);
-
-		for (i = 0; i < n; i++)
+		if (n > 0)
 		{
-			if (i && i % n)
-				printf("%s", separator);
+			va_start(ap, n);
 
-			printf("%d", va_arg(ap, int));
+			for (i = 0; i < n; i++)
+			{
+				if (i && i % n)
+					printf("%s", separator);
+
+				printf("%d", va_arg(ap, int));
+			}
+			printf("\n");
+			va_end(ap);
 		}
-		printf("\n");
-		va_end(ap);
 	}
 }
