651ca99be6b74c15f89c17993fbf54ba851b2b10
jasmin1002 <jasmineolofin@gmail.com>
2022-04-14 22:42:19 +0000

    print_numbers file contains print_numbers definition

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - numbers
+ * @separator: string argument
+ * @n: number of arguments (mechanism)
+ * ...: varying number of arguments of varying type.
+ *
+ * Return: void (nothing)
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	if (separator != NULL)
+	{
+		if (n == 0)
+			return;
+
+		va_start(ap, n);
+
+		for (i = 0; i < n; i++)
+		{
+			if (i && i % n)
+				printf("%s", separator);
+
+			printf("%d", va_arg(ap, int));
+		}
+		va_end(ap);
+		printf("\n");
+	}
+}
