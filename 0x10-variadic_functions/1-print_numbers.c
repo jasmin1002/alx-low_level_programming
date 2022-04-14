56ab58797c3ed52d89aff732071079d4bbde1da1
jasmin1002 <jasmineolofin@gmail.com>
2022-04-14 23:55:18 +0000

    null checking added

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,8 +13,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	if (separator != NULL)
+	if (n > 0)
 	{
+		if (separator == NULL)
+			return;
+
 		va_start(ap, n);
 
 		for (i = 0; i < n; i++)
