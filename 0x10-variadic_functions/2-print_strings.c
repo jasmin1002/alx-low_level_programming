d7e2d3bbe81cd9e869f9c4265ca6e6be83648e14
jasmin1002 <jasmineolofin@gmail.com>
2022-04-15 00:47:44 +0000

    header file updated with print_string proto and print_strings defines in print_strings.c file

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @separator: delimiter between words
+ * @n: argument counts (mechanism)
+ * ...: variable length of arguments
+ *
+ * Return: void (nothing)
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list ap;
+
+	if (n == 0)
+		return;
+
+	va_start(ap, n);
+
+	for (i = 0, str = va_arg(ap, char *); i < n; i++, str = va_arg(ap, char *))
+	{
+		if (i && i % n && separator != NULL)
+			printf("%s", separator);
+
+		if (str == NULL)
+			printf("(nil)");
+
+		else
+			printf("%s", str);
+	}
+	va_end(ap);
+
+	printf("\n");
+}
