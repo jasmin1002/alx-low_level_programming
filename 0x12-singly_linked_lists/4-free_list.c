f2970b12e07ca4e37f55631021c7f7cee058ba1a
jasmin1002 <jasmineolofin@gmail.com>
2022-04-22 02:27:16 +0000

    free_list function defines in 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - return memory space to OS
+ * @head: address of the list
+ *
+ * Return: void (nothing)
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+
+	free(head);
+}
