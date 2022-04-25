c9298f291061aa980f72b825804ee8939af6170b
Jasmine <jasmineolofin@gmail.com>
2022-04-25 09:45:15 +0000

    working in progress for assembly code task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -5,11 +5,12 @@ main:
 	mov edx, len	;message length
 	mov ecx, msg	;message to write
 	mov ebx, 1	;file descriptor (stdout)
-	mov eax, 4	;system call number (sys_write)
-	int 0x80	;call kernel
-	mov eax, 1	;system call number (sys_exit)
-	int 0x80	;call kernel
+	mov al, 0	;magic for varargs (0==no magic, to prevent a crash!)
 
+	extern printf
+	call printf
+
+	ret
 section .data
-msg db 'Hello, Holberton', 0xa	;our dear string
+msg db 'Hello, Holberton', 0	;our dear string
 len equ $ - msg			;length of our dear string
