c6f33fc1e36b3f798d7c13cbb3ae80843c795e56
Jasmine <jasmineolofin@gmail.com>
2022-04-25 09:36:47 +0000

    lists.h updated with macro and print_listint prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,11 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/* Standard libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -11,3 +19,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+size_t print_listint(const listint_t *);
+
+#endif
