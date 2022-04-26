df927d9b1ff7b43189e7abbb4adec2a191d885e0
Jasmine <jasmineolofin@gmail.com>
2022-04-26 08:59:06 +0000

    5-free_listint2.c defines free_listint2 function

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+  * free_listint2 - frees a node of list
+  * @head: address of the first node
+  *
+  * Return: void (nothing)
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *new_h_node = (*head)->next;
+	listint_t *tmp;
+
+	while (new_h_node)
+	{
+		tmp = new_h_node->next;
+		free(new_h_node);
+		new_h_node = tmp;
+	}
+
+	/*headnode->next = NULL;*/
+	free(*head);
+	*head = NULL;
+}
