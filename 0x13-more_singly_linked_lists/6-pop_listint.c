76f235ef3664854148e7fd15fb1214278e3ecf5b
Jasmine <jasmineolofin@gmail.com>
2022-04-26 11:11:42 +0000

    6-pop_listint.c defines pop_listint function

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+  * pop_listint - deletes the head node of a linked list
+  * @head: address of the first node
+  *
+  * Return: head node's data
+  */
+int pop_listint(listint_t **head)
+{
+	listint_t *newlist = (*head)->next;
+	int data;
+
+	if (*head == NULL)
+		return (0);
+
+	data = (*head)->n;
+	free(*head);
+
+	*head = newlist;
+
+	return (data);
+}
