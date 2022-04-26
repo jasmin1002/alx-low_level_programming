f98cbf86fe5264d1339e2fa4b1ecf965ac5a499f
Jasmine <jasmineolofin@gmail.com>
2022-04-26 09:24:35 +0000

    new line character added

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *head)
 	while (head)
 	{
 		if (head->str == NULL)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 
 		else
 			printf("[%d] %s\n", head->len, head->str);
