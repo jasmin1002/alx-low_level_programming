9e421a1cd0595e692b8093921519e959a5aa2efa
Jasmine <jasmineolofin@gmail.com>
2022-04-27 21:45:07 +0000

    Empty file for reverse_list added

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+  * reverse_listint - reverses a linked list
+  * @head: address of the first node
+  *
+  * Return: a pointer to the first node
+  */
+listint_t *reverse_listint(listint_t **head)
+{
+}
