a71c5c21fd51ff896572198e7ceda039f6c47a7a
Jasmine <jasmineolofin@gmail.com>
2022-06-09 20:34:07 +0000

    correct typo in header file: dlistints_s -> dlistint_s

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -18,8 +18,8 @@
 typedef struct dlistint_s
 {
 	int n;
-	struct dlistints_s *prev;
-	struct dlistints_s *next;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *);
