b833f462197329b7a177563c2ec192beadaf4c02
Jasmine <jasmineolofin@gmail.com>
2022-06-09 20:16:16 +0000

    print_dlistint function prints and counts the number of nodes in a list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+  * print_dlistint - prints data in linked node
+  * @head: head pointer
+  *
+  * Return: number of nodes in list
+  */
+size_t print_dlistint(const dlistint_t *head)
+{
+	unsigned int count = 0;
+
+	while (head)
+	{
+		printf("%d\n", head->n);
+		head = head->next;
+		count += 1;
+	}
+	return count;
+}
+
