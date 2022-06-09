3c6162b7308f6684878d8f31616562352d0b8af8
Jasmine <jasmineolofin@gmail.com>
2022-06-09 20:48:52 +0000

    1-dlistint_len.c produces the size of nodes in doubly linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/*
+ * dlistint_len - returns the size of given list
+ * @head: pointer to head node
+ *
+ * Return: number of node in a list
+ */
+size_t dlistint_len(const dlistint_t *head)
+{
+	unsigned int size = 0;
+
+	while (head)
+	{
+		head = head->next;
+		size += 1;
+	}
+
+	return size;
+}
