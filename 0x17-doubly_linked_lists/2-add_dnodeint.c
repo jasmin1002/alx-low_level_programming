4ef7e6226c8d90bfc3043bd32c6aaab8b71f4aa6
Jasmine <jasmineolofin@gmail.com>
2022-06-09 21:13:38 +0000

    2-add_dnodeint.c store a given data in a node from beginning

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning of a list
+ * @head: reference to the first node
+ * @n: data to store in new node
+ *
+ * Return: address of the new element
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		*head = new_node;
+	}
+	else
+	{
+		new_node->next = *head;
+		new_node->prev = (*head)->prev;
+		(*head)->prev = new_node;
+		*head = new_node;
+	}
+	return (new_node);
+}
