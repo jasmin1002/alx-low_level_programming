35666cc3bcc049cd23a0149d2507790f839551ce
Jasmine <jasmineolofin@gmail.com>
2022-10-03 09:16:38 -0700

    Added script for binary search algorithm template

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,16 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - search for a value by binary search on sorted array
+ * @array: sorted array
+ * @size: length of given array
+ * @value: target
+ *
+ * Return: index of target value (SUCCESS) or -1 (FAILURE)
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int found;
+
+	return (found);
+}
