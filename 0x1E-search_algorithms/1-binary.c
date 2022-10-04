83debc5eba092ad016b5136b342f1182c826aa5d
Jasmine <jasmineolofin@gmail.com>
2022-10-03 22:04:20 -0700

    Added some code to binary search template for testing purpose

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,7 +10,47 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int found;
+	int m, found = -1, lB = 0, uB = size;
+
+	while (lB < uB)
+	{
+		printf("Searching in array: ");
+		print_array(array, lB, uB);
+
+		m = (lB + uB) / 2;
+
+		if (array[m] < value)
+			lB = m + 1;
+		else if (array[m] > value)
+			uB = m;
+		else
+		{
+			found = m;
+			break;
+		}
+	}
 
 	return (found);
+}
+
+
+/**
+ * print_array - print an array
+ * @array: given array to print
+ * @size: length of given array
+ *
+ * Return: nothing
+ */
+void print_array(int *array, int lB, int uB)
+{
+	/*int len = uB - lB;*/
+
+	for (; lB < uB; lB++)
+	{
+		printf("%d", array[lB]);
+		if (lB != uB - 1)
+			printf(" ,");
+	}
+
+	printf("\n");
 }
