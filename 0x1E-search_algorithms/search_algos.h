e19cd0944cbbd999d8bd9a5143b03b96b293308b
Jasmine <jasmineolofin@gmail.com>
2022-10-04 10:22:01 -0700

    Added MIN macro and jump_search prototype to header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,10 +1,15 @@
 #ifndef SEARCH_ALGOS
 #define SEARCH_ALGOS
 
+#define MIN(x, y) ((x < y) ? x : y)
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int linear_search(int *, size_t, int);
 int binary_search(int *, size_t, int);
 void print_array(int *, int, int);
+int jump_search(int *, size_t, int);
+
+
 #endif
