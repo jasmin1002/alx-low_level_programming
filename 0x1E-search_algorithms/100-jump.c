ce4da818bfefc8f08d950e6baf652f91e22e2237
Jasmine <jasmineolofin@gmail.com>
2022-10-04 10:22:47 -0700

    Added script describing jump search algorithm template

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - searches for a target value in a sorted array
+ * @array: pointer to first element of array
+ * @size: length of the given array
+ * @value: target value
+ *
+ * Return: target value's index(Found) or -1 (Not Found)
+ */
+int jump_search(int *array, size_t size, int value)
+{
+}
