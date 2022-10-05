524f1e4568f7a586ba4699a28986914712250aad
Jasmine <jasmineolofin@gmail.com>
2022-10-05 05:34:49 -0700

    Added script for jump-search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -10,4 +10,44 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
+	int lB, tmp = 0, m = sqrt(size), len = size;
+
+	for (lB = 0; lB <= len; lB = MIN(lB, len))
+	{
+		if (array[lB] < value)
+		{
+			printf("Value checked array[%d] = [%d]\n", lB, array[lB]);
+			tmp = lB;
+			lB += m;
+		}
+		if (array[lB] >= value)
+		{
+			printf("Value found between indexes [%d] and [%d]\n", tmp, lB);
+			break;
+		}
+
+		if (lB > len)
+		{
+			printf("Value found between indexes [%d] and [%d]\n", tmp, lB);
+			break;
+		}
+	}
+
+	while (1)
+	{
+		printf("Value checked array[%d] = [%d]\n", tmp, array[tmp]);
+
+		if (array[tmp] == value)
+			break;
+
+		if (tmp == len - 1)
+		{
+			tmp = -1;
+			break;
+		}
+
+		tmp += 1;
+	}
+
+	return (tmp);
 }
